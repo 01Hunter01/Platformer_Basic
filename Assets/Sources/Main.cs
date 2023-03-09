605e5d960175df588bdbb96a568c6625609b363b
Artur <yermak01artur@gmail.com>
2023-03-09 14:09:55 +0200

    Added different effects, joints and water animation

--- Assets/Sources/Main.cs
+++ Assets/Sources/Main.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Plarformer
@@ -10,14 +10,16 @@ namespace Plarformer
         [SerializeField] private CannonView cannonView;
         [SerializeField] private Camera mainCamera;
         [SerializeField] private SpriteRenderer background;
-        
+        [SerializeField] private List<LevelObjectView> waterViews;
+
         private ParallaxController _parallaxController;
         private CameraController _cameraController;
         private PlayerController _playerController;
         private CoinController _coinController;
         private CannonController _cannonController;
         private EmitterController _emitterController;
-        
+
+        private WaterManager _waterManager;
         
         private void Awake()
         {
@@ -27,6 +29,8 @@ namespace Plarformer
             _cameraController = new CameraController(mainCamera.transform, playerView.transform);
             _cannonController = new CannonController(cannonView.muzzleT, playerView.trans);
             _emitterController = new EmitterController(cannonView.bullets, cannonView.emitterT);
+
+            _waterManager = new WaterManager(waterViews);
         }
 
         private void Update()
@@ -34,6 +38,7 @@ namespace Plarformer
             _coinController.Execute();
             _cannonController.Execute();
             _emitterController.Execute();
+            _waterManager.Execute();
         }
 
         private void FixedUpdate()
---
+++ Assets/Sources/Managers/WaterManager.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plarformer
+{
+    public class WaterManager
+    {
+        private float _animationSpeed = 8f;
+
+        private List<LevelObjectView> _waterViews;
+        private AnimationConfig _waterConfig;
+        private SpriteAnimatorController _waterAnimator;
+
+        public WaterManager(List<LevelObjectView> waterViews)
+        {
+            _waterViews = waterViews;
+            _waterConfig = Resources.Load<AnimationConfig>("SpriteWaterConfig");
+            _waterAnimator = new SpriteAnimatorController(_waterConfig);
+
+            foreach (var waterView in _waterViews)
+            {
+                _waterAnimator.StartAnimation(waterView.spriteRenderer, AnimState.Idle, true, _animationSpeed);
+            }
+        }
+
+        public void Execute()
+        {
+            _waterAnimator.Execute();
+        }
+    }
+}
